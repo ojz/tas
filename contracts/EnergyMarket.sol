45b53af2a442a48c009604fec1a830411145d9e7
Olmo Ziarko <olmo@ziarko.be>
2017-07-28 11:58:43 +0200

    buy orders, WIP

--- contracts/EnergyMarket.sol
+++ contracts/EnergyMarket.sol
@@ -79,47 +79,171 @@ contract EnergyMarket {
     }
 
     // sell 
-/*
+
     struct SellOrder {
+        address owner;
         uint maxKW;
         uint priceKW;
     }
 
-    mapping (address => SellOrder[]) sellOrders;
+    SellOrder[] sellOrders; // unsorted
 
     function sellOrder(uint maxKW, uint priceKW) {
         require(houseExists(msg.sender));
-        SellOrder storage sellOrder;
-        sellOrder = SellOrder(maxKW, priceKW);
 
-        if (sellOrders[msg.sender].length > 0) {
-            sellOrders[msg.sender].push(sellOrder);
-        } else {
-            sellOrders[msg.sender] = [sellOrder];
-        }
+        // remove from the balance of the house
+        var house = houses[msg.sender];
+        require(house.balanceKW >= maxKW);
+        house.balanceKW = house.balanceKW - maxKW;
+
+        // add the sellorder to the book
+        SellOrder memory so = SellOrder(msg.sender, maxKW, priceKW);
+        sellOrders.push(so);
     }
 
-    function cancelSellOrder(address owner, uint index) {
-        require(houseExists(owner));
-        require(msg.sender == companyAddress || msg.sender == owner);
+    function cancelSellOrder(uint index) {
+        require(houseExists(msg.sender));
+        
+        require(sellOrders.length > index);
+        var so = sellOrders[index];
+        require(so.owner == msg.sender);
+
+        // add the balance back
+        var house = houses[msg.sender];
+        house.balanceKW = house.balanceKW + so.maxKW;
+
+        // remove the sell order
+        sellOrders[index] = sellOrders[sellOrders.length - 1];
+        sellOrders.length--;
     }
 
     // buy
 
     struct BuyOrder {
-        address[] storage from;
-        uint[] storage amount;
+        uint companyAmount; // how much we will buy from the company.
+        uint[] orders; // indices of the sell orders used.
+        uint[] amounts; // amount of KW used per sell order.
     }
 
-    function buy() returns (uint index, uint totalPrice) {
+    BuyOrder[] buyOrders;
+
+    function buyOrder(uint totalKW) returns (uint totalPrice) {
+
         // make an initial buyOrder
-        // loop over the houseAddresses
-        // iteratively improve the buyOrder
+        var companyAmount = totalKW;
+        var mostExpensivePriceUsed = companyPrice;
+
+        uint[] orders; // both sorted by price
+        uint[] amounts;
+
+        // loop over the sellOrders, iteratively improve the buyOrder
+        for (uint i = 0; i < sellOrders.length; i++) {
+            var so = sellOrders[i];
+
+            // discard orders which are too expensive
+            if (so.priceKW >= mostExpensivePriceUsed) {
+                continue;
+            }
+
+            // locate the position where we will insert
+            var toAllocate = totalKW;
+            uint position;
+            for (position = 0; position < orders.length; position++) {
+                var currentPrice = sellOrders[orders[position]].priceKW;
+                if (currentPrice < so.priceKW) {
+                    toAllocate = toAllocate - amounts[position];
+                    continue;
+                }
+            }
+
+            // if we reached the end, push to the arrays and subtract from companyAmount.
+            if (position == orders.length - 1) {
+                orders.push(i);
+
+                var willAllocate = toAllocate > so.maxKW ? so.maxKW : toAllocate;
+                amounts.push(willAllocate);
+
+                // update how much we will need to buy from the company
+                companyAmount = toAllocate - willAllocate;
+
+                // check if we need to update the highest price we paid
+                if (companyAmount == 0) {
+                    mostExpensivePriceUsed = so.priceKW;
+                }
+
+                continue;
+            }
+
+            // make room for the order, moving all the more expensive orders to the right.
+            orders.length++;
+            amounts.length++;
+            for (uint j = orders.length - 2; j > position; j--) {
+                orders[j] = orders[j - 1];
+                amounts[j] = amounts[j - 1];
+            }
+
+            orders[position] = i;
+            amounts[position] = 0;
+
+            // update the amounts, starting from the current
+            for (; position < orders.length; position++) {
+                so = sellOrders[orders[position]];
+
+                willAllocate = toAllocate > so.maxKW ? so.maxKW : toAllocate;
+                amounts[position] = willAllocate;
+
+                toAllocate = toAllocate - willAllocate;
+                if (toAllocate == 0) {
+                    break;
+                }
+            }
+
+            // remove all left over orders
+            for (j = position + 1; j < orders.length; j++) {
+                delete orders[j];
+                delete amounts[j];
+            }
+
+            orders.length = position - 1;
+            amounts.length = position - 1;
+
+            // update the most expensive price used
+            if (toAllocate > 0) {
+                companyAmount = toAllocate;
+                mostExpensivePriceUsed = companyPrice;
+            } else {
+                companyAmount = 0;
+                mostExpensivePriceUsed = sellOrders[orders[orders.length - 1]].priceKW;
+            }
+        }
+
+        // calculate the total price of the buy order
+        totalPrice = companyPrice * companyAmount;
+        for (i = 0; i < orders.length; i++) {
+            totalPrice = totalPrice + (amounts[i] * sellOrders[orders[i]].priceKW);
+        }
+
+        // update the sell orders that we used
+        for (i = 0; i < orders.length; i++) {
+            var order = sellOrders[orders[i]];
+            order.maxKW = order.maxKW - amounts[i];
+        }
+
+        // store the buy order
+        BuyOrder memory bo = BuyOrder(companyAmount, orders, amounts);
+        buyOrders.push(bo);
     }
 
-    function pay(uint index) {
+/*
+    function payBuyOrder(uint index) {
+    }
+
+    function cancelBuyOrder() {
+        // put the balance back in the sell orders
+        // if a sell order is closed, put it in the house.
     }
 */
+
     // withdraw
 
     mapping (address => uint) balances;
